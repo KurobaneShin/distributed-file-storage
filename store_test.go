336aa5356c2ce509bd54738997a9240edb69e347
icaro <darkflamemaster663@gmail.com>
2024-04-28 22:17:55 -0300

    start store module and test

---
+++ store_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestStore(t *testing.T) {
+	opts := StoreOpts{
+		PathTransformFunc: DefaultPathTransformFunc,
+	}
+
+	s := NewStore(opts)
+
+	data := bytes.NewReader([]byte("some jpeg bytes"))
+
+	if err := s.writeStream("myspecialpicture", data); err != nil {
+		t.Error(err)
+	}
+}
