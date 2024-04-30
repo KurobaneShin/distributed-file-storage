bc182783fe7101e62d8e30ea35b8f936058b0463
kurobane <darkflamemaster663@gmail.com>
2024-04-29 21:08:02 -0300

    add read stream and CASPath

--- store.go
+++ store.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
-type PathTransformFunc func(string) string
+func CASPathTransformFunc(key string) PathKey {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blocksize := 5
+	sliceLen := len(hashStr) / blocksize
+
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blocksize, (i*blocksize)+blocksize
+		paths[i] = hashStr[from:to]
+
+	}
+
+	return PathKey{PathName: strings.Join(paths, "/"), Filename: hashStr}
+}
+
+type PathTransformFunc func(string) PathKey
+
+type PathKey struct {
+	PathName string
+	Filename string
+}
+
+func (p PathKey) FullPath() string {
+	return fmt.Sprintf("%s/%s", p.PathName, p.Filename)
+}
 
 type StoreOpts struct {
 	PathTransformFunc PathTransformFunc
@@ -26,16 +58,33 @@ func NewStore(opts StoreOpts) *Store {
 	}
 }
 
-func (s *Store) writeStream(key string, r io.Reader) error {
-	pathName := s.PathTransformFunc(key)
+func (s *Store) Read(key string) (io.Reader, error) {
+	f, err := s.readStream(key)
+	if err != nil {
+		return nil, err
+	}
 
-	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+	defer f.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, f)
+
+	return buf, err
+}
+
+func (s *Store) readStream(key string) (io.ReadCloser, error) {
+	pathKey := s.PathTransformFunc(key)
+	return os.Open(pathKey.FullPath())
+}
+
+func (s *Store) writeStream(key string, r io.Reader) error {
+	pathKey := s.PathTransformFunc(key)
+
+	if err := os.MkdirAll(pathKey.PathName, os.ModePerm); err != nil {
 		return err
 	}
 
-	filename := "somefilename"
-
-	pathAndFilename := pathName + "/" + filename
+	pathAndFilename := pathKey.FullPath()
 
 	f, err := os.Create(pathAndFilename)
 	if err != nil {
--- store_test.go
+++ store_test.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestPathTransformFnc(t *testing.T) {
+	key := "momsbestpicture"
+
+	PathName := CASPathTransformFunc(key)
+
+	assert.Equal(t, PathName.PathName, "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff")
+	assert.Equal(t, PathName.Filename, "6804429f74181a63c50c3d81d733a12f14a353ff")
+}
 
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 
 	s := NewStore(opts)
+	key := "momsspecials"
+	data := ([]byte("some jpeg bytes"))
 
-	data := bytes.NewReader([]byte("some jpeg bytes"))
-
-	if err := s.writeStream("myspecialpicture", data); err != nil {
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
 		t.Error(err)
 	}
+
+	r, err := s.Read(key)
+	assert.Nil(t, err)
+
+	b, _ := io.ReadAll(r)
+
+	assert.Equal(t, string(b), string(data))
 }
